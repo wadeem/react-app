5ad3970e5a7639b2b8a11418c6ad5f19ea50971b
wadeem <vadim@unikivi.ee>
2020-04-18 11:43:56 +0300

    cicd check

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello world!</h2>
+        <p>This is something about CI/CD</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
