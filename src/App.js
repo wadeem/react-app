b33715a28759cd88c972254ed790608a736e0b32
wadeem <vadim@unikivi.ee>
2020-04-19 13:25:21 +0300

    cicd check 19.04.2020

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>19.04.2020 edition</p>
         <a
           className="App-link"
           href="https://reactjs.org"
